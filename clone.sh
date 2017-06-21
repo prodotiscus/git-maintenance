d1274bd543bf014aa939026c7227f5943bea0c13
prodotiscus <f.sizov@yandex.ru>
2017-06-21 17:43:56 +0300

    minor update

--- clone.sh
+++ clone.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-git clone "https://github.com/prodotiscus/$1"
+if [ -z "$2" ];
+then
+	git clone "https://github.com/prodotiscus/$1"
+else
+	git clone "https://github.com/$2/$1"
+fi
+
 cd "$1"
 git config user.name prodotiscus
 git config user.email f.sizov@yandex.ru
