9b74a52db7af6bf8475361cb0e22a1865f3053e8
Fyodor Sizov <f.sizov@yandex.ru>
2019-03-21 21:08:20 +0300

    +getSecurityHistory

---
+++ getSecurityHistory.js
@@ -0,0 +1,19 @@
+function getSecurityHistory() {
+  var brp2 = document.querySelectorAll(".Box-row.p-2");
+  var history = [];
+  for (var k = 0; k < brp2.length; k ++) {
+    var datetime = brp2[k].querySelector(".security-history-timestamp relative-time").getAttribute("datetime");
+    var local = {};
+    var keys = brp2[k].querySelectorAll("th");
+    var values = brp2[k].querySelectorAll("tr");
+    for (var j = 0; j < keys.length; j ++) {
+      local[keys[j]] = values[j];
+    }
+    history.push({
+      event: local,
+      datetime: datetime
+    });
+  }
+
+  return history;
+}
